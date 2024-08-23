849410ae50615e09928e4279bdfe3f2c2c5e7fe0
Agboola Michael Daramola <agbooladaramola7@gmail.com>
2024-08-23 10:10:54 +0100

    Positive Scenarios Test creation

--- cypress/e2e/Contact_Us.feature
+++ cypress/e2e/Contact_Us.feature
@@ -3,8 +3,8 @@ Feature: WebdriverUniversity - Contact Us Page
         Given I navigate the WebdriverUniversity homepage
         When I click on the Contact us button
         And I type a first name
-        # And I type a last name
-        # And I enter an email address
-        # And I type a comment
-        # And I click on the submit button
-        # Then The system should display a Successful message as a feedback
+        And I type a last name
+        And I enter an email address
+        And I type a comment
+        And I click on the submit button
+        Then The system should display a Successful message as a feedback
--- cypress/support/step_definitions/Contactpage_Steps.js
+++ cypress/support/step_definitions/Contactpage_Steps.js
@@ -1,7 +1,22 @@
 /// <reference types="cypress" />
 
-import { When } from "@badeball/cypress-cucumber-preprocessor";
+import { Then, When } from "@badeball/cypress-cucumber-preprocessor";
 
 When(`I type a first name`, ()=>{
-    cy.get('input[name="first_name"]').type("Agboola Michael")
+    cy.get('input[name="first_name"]').type("Agboola Michael");
+})
+When(`I type a last name`, ()=>{
+    cy.get('input[name="last_name"]').type("Daramola")
+})
+When(`I enter an email address`, ()=>{
+    cy.get('input[name="email"]').type("agbooladaramola7@gmail.com")
+})
+When(`I type a comment`, ()=>{
+    cy.get('textarea[name="message"]').type("Testing 1, 2, 3...")
+})
+When(`I click on the submit button`, ()=>{
+    cy.get('input[type="submit"]').type("Daramola")
+})
+Then(`The system should display a Successful message as a feedback`, ()=>{
+    cy.get('h1').should('have.text','Thank You for your Message!');
 })
