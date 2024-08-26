a53d975c71ddb04d822357d5d078a37acedd7828
Agboola Michael Daramola <agbooladaramola7@gmail.com>
2024-08-26 11:29:13 +0100

    Updated Cypress version with resolution to the Example features

--- cypress/support/step_definitions/Loginpage_Steps.js
+++ cypress/support/step_definitions/Loginpage_Steps.js
@@ -4,17 +4,17 @@ import { Then, When } from "@badeball/cypress-cucumber-preprocessor";
 
 let stub;
 
-When(`I type the username {}`, (username)=>{
-    cy.get('#text').type(username);
+When('I type the username {}', (username)=>{
+    cy.get('#text').type(username,{delay: 100});
 })
-When(`I type the password {}`,(password)=>{
-    cy.get('#password').type(password);
+When('I type the password {}',(password)=>{
+    cy.get('#password').type(password, {delay: 100});
 })
-When(`I Click on the Login button`, ()=>{
+When('I Click on the Login button', ()=>{
     stub = cy.stub();
     cy.on('window:alert', stub);
     cy.get('#login-button').click();
 })
-Then(`The system should display {string}`,(expectedAlertText)=>{
+Then('The system should display {string}',(expectedAlertText)=>{
     expect(stub).to.have.been.calledWith(expectedAlertText);
 })
